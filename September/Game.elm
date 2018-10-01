843c7a19d399bd68eb90981df298cf678d751afe
Daniel Miller <danm1113@gmail.com>
2018-09-30 17:18:05 -0700

    Initial setup complete

--- September/Game.elm
+++ September/Game.elm
@@ -1,3 +1,58 @@
 module Game exposing (..)
+import Html exposing (Html, program)
+import Dict exposing (Dict(..), empty)
 
-one = 1
+type alias Position = (Int, Int)
+
+type alias ID = Int
+
+type alias Model =
+  { balls : List ID
+  , blocks : List ID
+  , field : Dict Position ID
+  , gameState : GameState
+  , paddles : List ID
+  , powerUps : List ID
+  }
+
+type Msg
+  = Nop
+
+type GameState
+  = Menu
+  | GameStart
+  | Playing
+  | GameOver
+
+main : Program Never Model Msg
+main =
+  program
+    { init = init
+    , subscriptions = subscriptions
+    , update = update
+    , view = view
+    }
+
+init : (Model, Cmd Msg)
+init =
+  ({ balls = []
+  , blocks = []
+  , field = empty
+  , gameState = Menu
+  , paddles = []
+  , powerUps = []
+  }, Cmd.none)
+
+view : Model -> Html.Html Msg
+view state =
+  Html.div [] [
+    Html.text "Test"
+  ]
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg state =
+  (state, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions state =
+  Sub.none
